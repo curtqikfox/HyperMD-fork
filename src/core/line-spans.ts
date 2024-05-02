5d1438a14ddc9bb453fca1190a4f810eacd09b13
curtqikfox <curtis@qikfox.com>
2024-05-02 12:40:23 -0700

    changing definition of indent

--- src/core/line-spans.ts
+++ src/core/line-spans.ts
@@ -17,7 +17,7 @@ export interface Span {
   end: number
 }
 
-type SpanType = "em" | "strong" | "strikethrough" | "code" | "linkText" | "linkHref" | "task" | "hashtag" | "customLink" | "indentation"
+type SpanType = "em" | "strong" | "strikethrough" | "code" | "linkText" | "linkHref" | "task" | "hashtag" | "customLink" | "indent"
 
 const enum SpanAction {
   NOTHING = 0,
@@ -91,8 +91,8 @@ class LineSpanExtractor {
       // customLink
       customLink: ((token.type !== null && token.type != undefined ? token.type.indexOf("hmd-customlink-begin") != -1 : 0) ? 1 /* SpanAction.IS_THIS_TYPE */
       : ((token.type !== null && token.type != undefined ? token.type.indexOf("hmd-customlink-end") != -1 : 0)) ? 2 /* SpanAction.LEAVING_THIS_TYPE */ : 0 /* SpanAction.NOTHING */),
-      // Indentation
-      indentation: (token.type !== null && token.type != undefined && token.type.indexOf('hmd-indent') !== -1) ? 1 /* SpanAction.IS_THIS_TYPE */
+      // Indent
+      indent: (token.type !== null && token.type != undefined && token.type.indexOf('hmd-indent') !== -1) ? 1 /* SpanAction.IS_THIS_TYPE */
       : (0 /* SpanAction.NOTHING */),
     }
     return ans
