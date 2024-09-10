f82412e1108e8dc0bafd11195a203e87f38364e4
arun-qikfox <75040337+arun-qikfox@users.noreply.github.com>
2024-09-10 17:17:56 +0530

    Image and youtube video resizing is done and draggable enabled

--- src/addon/fold-image.ts
+++ src/addon/fold-image.ts
@@ -1,132 +1,139 @@
 // HyperMD, copyright (c) by laobubu
 // Distributed under an MIT license: http://laobubu.net/HyperMD/LICENSE
 //
-// DESCRIPTION: Fold Image Markers `![](xxx =width*height align)`
+// DESCRIPTION: Fold Image Markers `![](xxx)`
 //
 
 import { FolderFunc, registerFolder, RequestRangeResult, breakMark } from "./fold";
 import { Position } from "codemirror";
 import { splitLink } from "./read-link";
+import interact from 'interactjs';
 
 const DEBUG = false
 
-export const ImageFolder: FolderFunc = function (stream, token) {
-  const cm = stream.cm
-  const youtubeUrlRE = /^(https?:\/\/)?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)([\w-]{11})(.*)?$/;
-  const imgRE = /\bimage-marker\b/
-  const urlRE = /\bformatting-link-string\b/   // matches the parentheses
-  const sizeAlignRE = /(?: =(\d+)?\*?(\d+)?\s*(left|center|right)?)?$/;  // matches the size " =width*height align"
 
+const youtubeUrlRE = /^(https?:\/\/)?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)([\w-]{11})(.*)?$/;
+const imgRE = /\bimage-marker\b/;
+const urlRE = /\bformatting-link-string\b/;   // matches the parentheses
+const sizeAlignRE = /(?: =(\d+)?\*?(\d+)?\s*(left|center|right)?)?$/;  // matches the size " =width*height align"
+
+export const ImageFolder: FolderFunc = function (stream, token, enableResizeAndDrag = true) {
+  const cm = stream.cm;
+  
   if (imgRE.test(token.type) && token.string === "!") {
-    var lineNo = stream.lineNo
+    var lineNo = stream.lineNo;
 
     // find the begin and end of url part
-    var url_begin = stream.findNext(urlRE)
-    var url_end = stream.findNext(urlRE, url_begin.i_token + 1)
+    var url_begin = stream.findNext(urlRE);
+    var url_end = stream.findNext(urlRE, url_begin.i_token + 1);
 
-    let from: Position = { line: lineNo, ch: token.start }
-    let to: Position = { line: lineNo, ch: url_end.token.end }
-    let rngReq = stream.requestRange(from, to, from, from)
+    let from = { line: lineNo, ch: token.start };
+    let to = { line: lineNo, ch: url_end.token.end };
+    let rngReq = stream.requestRange(from, to, from, from);
 
     if (rngReq === RequestRangeResult.OK) {
-      var url: string
-      var title: string
-      var width: number = null
-      var height: number = null
-      var align: string = null
+      var url;
+      var title;
+      var width = null;
+      var height = null;
+      var align = null;
 
-      { // extract the URL
-        let rawurl = cm.getRange(    // get the URL or footnote name in the parentheses
-          { line: lineNo, ch: url_begin.token.start + 1 },
-          { line: lineNo, ch: url_end.token.start }
-        )
-        if (url_end.token.string === "]") {
-          let tmp = cm.hmdReadLink(rawurl, lineNo)
-          if (!tmp) return null // Yup! bad URL?!
-          rawurl = tmp.content
-        }
-        url = splitLink(rawurl).url
-        url = cm.hmdResolveURL(url)
-        
-        // Check if there is size or alignment information
-        const sizeAlignMatch = sizeAlignRE.exec(rawurl)
-        if (sizeAlignMatch) {
-          width = sizeAlignMatch[1] ? parseInt(sizeAlignMatch[1], 10) : null
-          height = sizeAlignMatch[2] ? parseInt(sizeAlignMatch[2], 10) : null
-          align = sizeAlignMatch[3] || null
-          url = rawurl.replace(sizeAlignRE, '').trim() // Remove size and alignment info from the URL
-        }
+      // extract the URL
+      let rawurl = cm.getRange(
+        { line: lineNo, ch: url_begin.token.start + 1 },
+        { line: lineNo, ch: url_end.token.start }
+      );
+      if (url_end.token.string === "]") {
+        let tmp = cm.hmdReadLink(rawurl, lineNo);
+        if (!tmp) return null;
+        rawurl = tmp.content;
+      }
+      url = splitLink(rawurl).url;
+      url = cm.hmdResolveURL(url);
 
-        /********************** youtube embedding starts here ************************/
-        if(youtubeUrlRE.test(rawurl)) {
-          // Extract YouTube ID using regex
-          const youtubeMatch = youtubeUrlRE.exec(url);
-          if (!youtubeMatch) return null; // If not a valid YouTube URL, skip
+      // Check if there is size or alignment information
+      const sizeAlignMatch = sizeAlignRE.exec(rawurl);
+      if (sizeAlignMatch) {
+        width = sizeAlignMatch[1] ? parseInt(sizeAlignMatch[1], 10) : null;
+        height = sizeAlignMatch[2] ? parseInt(sizeAlignMatch[2], 10) : null;
+        align = sizeAlignMatch[3] || null;
+        url = rawurl.replace(sizeAlignRE, '').trim(); // Remove size and alignment info from the URL
+      }
 
-          var videoID = youtubeMatch[4]; // Extract the video ID
-
-          // Create the iframe element for embedding YouTube video
-          var youtubeIframe = document.createElement("iframe");
-          var videoHolder = document.createElement("div");
-          videoHolder.appendChild(youtubeIframe);
-          var youtubeMarker = cm.markText(
-            from, to,
-            {
-              clearOnEnter: true,
-              collapsed: true,
-              handleMouseEvents: true,
-              replacedWith: videoHolder,
-            }
-          );
-
-          youtubeIframe.src = `https://www.youtube.com/embed/${videoID}?rel=0`;
-          youtubeIframe.width = width?width.toString():"560";
-          youtubeIframe.height = height?height.toString():"315";
-          youtubeIframe.style.border = "0";
-          youtubeIframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
-          youtubeIframe.allowFullscreen = true;
-          youtubeIframe.title = title;
-          
-          videoHolder.style.position = "relative";
-          videoHolder.style.zIndex = "99";
-          videoHolder.className = "hmd-ytube"
-
-          // Add click event to break the marker
-          videoHolder.addEventListener('click', () => breakMark(cm, youtubeMarker), false);
-
-          if (align) {
-            if (align === "left") {
-              videoHolder.style.float = "left"
-              videoHolder.style.paddingRight = "20px"
-            } else if (align === "right") {
-              videoHolder.style.float = "right"
-              videoHolder.style.paddingLeft = "20px"
-            } else if (align === "center") {
-              videoHolder.style.display = "block"
-              videoHolder.style.marginLeft = "auto"
-              videoHolder.style.marginRight = "auto"
-            }
-          } else {
-            // default to set to left
-              videoHolder.style.display = "block"
-              videoHolder.style.width = "fit-content"
-              videoHolder.style.marginLeft = "auto"
-              videoHolder.style.marginRight = "auto"
+      // YouTube embedding
+      if (youtubeUrlRE.test(rawurl)) {
+        const youtubeMatch = youtubeUrlRE.exec(url);
+        if (!youtubeMatch) return null;
+        var videoID = youtubeMatch[4];
+        var youtubeIframe = document.createElement("iframe");
+        var videoHolder = document.createElement("div");
+        var mask = document.createElement("div");
+        videoHolder.appendChild(youtubeIframe);
+        videoHolder.appendChild(mask);
+        var youtubeMarker = cm.markText(
+          from, to,
+          {
+            clearOnEnter: true,
+            collapsed: true,
+            replacedWith: videoHolder,
           }
+        );
 
-          return youtubeMarker;
-        }
-        /********************** End: youtube embedding starts here ************************/
+        // mask
+        mask.style.position = "absolute";
+        mask.style.top = "0";
+        mask.style.left = "0";
+        mask.style.width = "100%";
+        mask.style.height = "100%";
+        mask.style.zIndex = "9999";
+        mask.style.display = "none";
+        
+        // player
+        youtubeIframe.src = `https://www.youtube.com/embed/${videoID}?rel=0`;
+        youtubeIframe.width = "100%"
+        youtubeIframe.height = "100%";
+        // youtubeIframe.style.border = "solid 2px transparent";
+        youtubeIframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+        youtubeIframe.allowFullscreen = true;
+        youtubeIframe.title = title;
+
+        // container
+        videoHolder.className = "hmd-ytube";
+        videoHolder.style.border = "solid 2px transparent";
+        videoHolder.style.width = width ? (width.toString()+"px") : "560px";
+        videoHolder.style.height = height ? (height.toString()+"px") : "315px";
+        videoHolder.style.display = "inline-block";
+        videoHolder.style.padding = "5px";
+
+        videoHolder.addEventListener('mouseover', () => {
+          // Show the dotted border
+          
+          // Apply dotted border and enable resizing and dragging if the feature is enabled
+          if (enableResizeAndDrag) {
+            videoHolder.style.border = "2px dotted #000";  
+        
+            // Setup the image for resizing and dragging
+            setupResizableAndDraggable(videoHolder, enableResizeAndDrag, cm, from, to, false, mask);
+          }
+        }, false);
+  
+        videoHolder.addEventListener('mouseout', () => {
+          videoHolder.style.border = "2px dotted transparent";
+        });
+        
+        setElementAlignment(videoHolder, align)
+
+        return youtubeMarker;
       }
 
-      { // extract the title
-        title = cm.getRange(
-          { line: lineNo, ch: from.ch + 2 },
-          { line: lineNo, ch: url_begin.token.start - 1 }
-        )
-      }
+      // Extract the title for image
+      title = cm.getRange(
+        { line: lineNo, ch: from.ch + 2 },
+        { line: lineNo, ch: url_begin.token.start - 1 }
+      );
 
-      var img = document.createElement("img")
+      // Create and handle image element
+      var img = document.createElement("img");
       var marker = cm.markText(
         from, to,
         {
@@ -134,52 +141,252 @@ export const ImageFolder: FolderFunc = function (stream, token) {
           collapsed: true,
           replacedWith: img,
         }
-      )
+      );
 
+      img.src = url;
+      img.title = title;
+      img.className = "hmd-image hmd-image-loading";
+      img.style.border = "solid 2px transparent"
+      img.style.padding = "5px";
+      
+      if (width) img.width = width;
+      if (height) img.height = height;
+
+      // Add load and error handling
       img.addEventListener('load', () => {
-        img.classList.remove("hmd-image-loading")
-        marker.changed()
-      }, false)
+        img.classList.remove("hmd-image-loading");
+        marker.changed();
+      }, false);
       img.addEventListener('error', () => {
-        img.classList.remove("hmd-image-loading")
-        img.classList.add("hmd-image-error")
-        marker.changed()
-      }, false)
-      img.addEventListener('click', () => breakMark(cm, marker), false)
+        img.classList.add("hmd-image-error");
+        marker.changed();
+      }, false);
 
-      img.className = "hmd-image hmd-image-loading"
-      img.src = url
-      img.title = title
-      if (width) img.width = width
-      if (height) img.height = height
-      if (align) {
-        if (align === "left") {
-          img.style.float = "left"
-          img.style.paddingRight = "20px"
-        } else if (align === "right") {
-          img.style.float = "right"
-          img.style.paddingLeft = "20px"
-        } else if (align === "center") {
-          img.style.display = "block"
-          img.style.marginLeft = "auto"
-          img.style.marginRight = "auto"
+      // Click event for resizing and dragging
+      img.addEventListener('mouseover', () => {
+        // Show the dotted border
+        
+        // Apply dotted border and enable resizing and dragging if the feature is enabled
+        if (enableResizeAndDrag) {
+          img.style.border = "2px dotted #000";  
+      
+          // Setup the image for resizing and dragging
+          setupResizableAndDraggable(img, enableResizeAndDrag, cm, from, to);
         }
-      } else {
-        // default set as float left
-        img.style.display = "block"
-        img.style.marginLeft = "auto"
-        img.style.marginRight = "auto"
-      }
+      }, false);
 
-      return marker
-    } else {
-      if (DEBUG) {
-        console.log("[image]FAILED TO REQUEST RANGE: ", rngReq)
-      }
+      img.addEventListener('mouseout', () => {
+        img.style.border = "2px dotted transparent";
+      });
+      setElementAlignment(img, align)
+
+      return marker;
     }
   }
 
-  return null
+  return null;
+};
+
+registerFolder("image", ImageFolder, true);
+
+function setupResizableAndDraggable(element, enableResizeAndDrag, cm, from, to, maintainAspectRatio=true, mask=null) {
+  if (!enableResizeAndDrag) return;
+
+  // Set the image style for resizing and dragging
+  element.style.position = "relative";
+  element.style.cursor = "move";
+  element.style.border = "2px dotted transparent";  // Default border, visible on click
+
+  // Add click event to show dotted border for resizing/dragging
+  element.addEventListener('mouseover', () => {
+    element.style.border = "2px dotted rgba(0, 0, 0, 0.6)"; // Show dotted border when clicked
+  }, false);
+
+  // Initialize interactjs for resizing
+  interact(element)
+    .resizable({
+      edges: { left: true, right: true, bottom: true, top: false },
+      listeners: {
+        start(event) {
+          if(mask) {
+            mask.style.display = "block";
+          }
+        },
+        move(event) {
+          let { width, height } = event.rect;
+          width = Math.round(width); 
+          height = Math.round(height);
+          element.style.width = `${width}px`;
+          element.style.height = `${height}px`;
+        },
+        end(event) {
+          if(mask) {
+            mask.style.display = "none";
+          }
+          let { width, height } = event.rect;
+          width = Math.round(width);
+          height = Math.round(height);
+          updateMarkdownSize(cm, from, to, width, height);
+        }
+      },
+      modifiers: [
+        interact.modifiers.restrictSize({
+          min: { width: 50, height: 50 }
+        }),
+        interact.modifiers.aspectRatio({ ratio: 'preserve', enabled: maintainAspectRatio }) // Preserve aspect ratio
+      ],
+      inertia: true
+    })
+    .draggable({
+      lockAxis: 'x',
+      listeners: {
+        move(event) {
+          
+          const position = { x: parseFloat(element.dataset.x) || 0, y: parseFloat(element.dataset.y) || 0 };
+
+          // Update the element position
+          position.x += event.dx;
+          position.y += event.dy;
+
+          element.style.transform = `translate(${position.x}px, ${position.y}px)`;
+
+          // Save the x and y positions in the dataset
+          element.dataset.x = position.x;
+          element.dataset.y = position.y;
+
+          // Update the alignment in markdown based on the dragged position
+          
+        },
+        end(event) {
+          const position = { x: parseFloat(element.dataset.x) || 0, y: parseFloat(element.dataset.y) || 0 };
+
+          // Update the element position
+          position.x += event.dx;
+          position.y += event.dy;
+
+          element.style.transform = `translate(${position.x}px, ${position.y}px)`;
+
+          // Save the x and y positions in the dataset
+          element.dataset.x = position.x;
+          element.dataset.y = position.y;
+          updateMarkdownAlignment(cm, from, to, element, position);
+        }
+      },
+      inertia: true
+    });
+  }
+
+// Utility function to update the size in the markdown
+function updateMarkdownSize(cm, from, to, width, height, align=null) {
+  let prevWidth = 0;
+  let prevHeight = 0;
+  let prevAlign = '';
+  // Get the current content in the marked range
+  const content = cm.getRange(from, to);
+  // existing data
+  const regex = /\(([^)]+)\)/;
+  const urlMatch = content.match(regex);
+  
+  // Regular expression to identify and extract the URL within the markdown image syntax
+  const urlPattern = /\!\[([^\]]*)\]\(([^)\s]+)(?:\s*=?\d*\*?\d*\s*(?:left|center|right)?)?\)/;
+  
+  // Match the current markdown syntax to extract the text and the URL
+  const match = urlPattern.exec(content);
+  if (!match) return; // If no match is found, exit the function
+
+  const altText = match[1]; // Alternative text inside []
+  const url = match[2]; // URL inside ()
+
+  if(urlMatch) {
+    const sizeAlignMatch = sizeAlignRE.exec(urlMatch[1]);
+    if (sizeAlignMatch) {
+      prevWidth = sizeAlignMatch[1] ? parseInt(sizeAlignMatch[1], 10) : null;
+      prevHeight = sizeAlignMatch[2] ? parseInt(sizeAlignMatch[2], 10) : null;
+      prevAlign = sizeAlignMatch[3] || null;
+    }
+  }
+  
+  // Rebuild the markdown string with updated size and alignment
+  let updatedMarkdown = `![${altText}](${url}`;
+
+  // Append size if provided
+  if (width || height) {
+    updatedMarkdown += ` =${width || ''}*${height || ''}`;
+  } else {
+    updatedMarkdown += ` =${prevWidth || ''}*${prevHeight || ''}`;
+  }
+
+  // Append alignment if provided
+  if (align) {
+    updatedMarkdown += ` ${align}`;
+  } else {
+    updatedMarkdown += ` ${prevAlign}`;
+  }
+
+  // Close the markdown with a closing parenthesis
+  updatedMarkdown += ")\n";
+  // Replace the existing content with the updated markdown
+  cm.replaceRange(updatedMarkdown, from, to);
 }
 
-registerFolder("image", ImageFolder, true)
+function setElementAlignment(element, alignment="center") {
+  if(alignment==="left") {
+    alignment = 'left';
+    element.style.float = 'left';
+    element.style.marginRight = "20px";
+  } else if(alignment==="right") {
+    alignment = 'right';
+    element.style.float = 'right';
+    element.style.marginLeft = "20px";
+  } else {
+    alignment = 'center';
+    element.style.display = 'block';
+    element.style.marginLeft = 'auto';
+    element.style.marginRight = 'auto';
+  }
+}
+
+
+// Utility function to update alignment in the markdown
+function updateMarkdownAlignment(cm, from, to, element, position) {
+  const parentWidth = element.closest('pre').offsetWidth;
+  let alignment;
+
+  // Determine current float alignment
+  const float = getComputedStyle(element).float;
+  let left = position.x;
+  if (float === 'right') {
+    if(position.x < 0) {  
+      left = -1 * position.x;
+      alignment = "right";
+    }
+    console.log(left, parentWidth*0.66)
+    if(left>parentWidth*0.66) {
+      alignment = "left";
+    } else if(left>parentWidth*0.33) {
+      alignment = "center";
+    } else {
+      // if it is dragged to right, nothing to be done
+      alignment = "right"
+    }
+  } else if (float === 'left') {
+    if (position.x > parentWidth * 0.66) {
+      alignment = "right"
+    } else if(position.x > parentWidth * 0.33) {
+      alignment = "center"
+    } else {
+      alignment = "left"
+    }
+  } else if (float === 'none') {
+    // Calculate alignment when float is none
+    if (position.x < 0) {
+      alignment = 'left';
+    } else if (position.x > 0) {
+      alignment = 'right';
+    } else {
+      alignment = 'center';
+    }
+  }
+  
+  updateMarkdownSize(cm, from, to, null, null, alignment);
+}
