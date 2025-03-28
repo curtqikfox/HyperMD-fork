5b97c3a5ade3962beea87c9f845787959b63cd73
arun-qikfox <75040337+arun-qikfox@users.noreply.github.com>
2025-03-28 18:03:26 +0530

    Fix: Copy paste of tables handled

--- src/powerpack/paste-with-turndown.ts
+++ src/powerpack/paste-with-turndown.ts
@@ -10,38 +10,38 @@
 // This module also supports GFM extensions (eg. tables) via [turndown-plugin-gfm](https://www.npmjs.com/package/turndown-plugin-gfm).
 // Load `turndown-plugin-gfm` after this module and it will be automatically used while pasting.
 //
+import TurndownService from "turndown";
+import * as turndownPluginGfm from "turndown-plugin-gfm"; // Import the plugin
+import { PasteConvertor, suggestedOption, defaultOption } from "../addon/paste";
 
-import TurndownService from "turndown"
-import { PasteConvertor, suggestedOption, defaultOption } from "../addon/paste"
+export const TurndownConvertor: PasteConvertor = (html) => {
+  console.log('HTML before processing:', html); // Log the HTML before processing
 
-export const TurndownConvertor: PasteConvertor =
-  (html) => {
-    // strip <a> without href
-    html = html.replace(/<a([^>]*)>(.*?)<\/a>/ig, (s, attrs, content) => {
-      if (!/href=/i.test(attrs)) return content
-      return s
-    })
-    // maybe you don't need to convert, if there is no img/link/header...
-    if (!/\<(?:hr|img|h\d|strong|em|strikethrough|table|a|b|i|del|ol|ul)(?:\s.*?|\/)?\>/i.test(html)) return null
-    
-    const turndownService = getTurndownService()
-    if (turndownService) {
-      let markdown = turndownService.turndown(html)
-      markdown = markdown.replace(/^ {4}/gm, "\t");
-      // the below is to convert 2 spaces or 3 spaces into tab since different editors has different combinations
-      // the tab controls will look consistent that way
-      markdown = markdown
-      .replace(/^ {4}/gm, "\t")        // 4 spaces to tab
-      .replace(/^ {3}/gm, "\t")        // 3 spaces to tab
-      .replace(/^(?!\t)( {2})/gm, "\t"); // 2 spaces to tab, but only if not already tabbed
-      return markdown;
-    }
+  // Strip <a> without href
+  html = html.replace(/<a([^>]*)>(.*?)<\/a>/ig, (s, attrs, content) => {
+    if (!/href=/i.test(attrs)) return content;
+    return s;
+  });
 
-    return null
+  // Maybe you don't need to convert if there is no relevant HTML
+  if (!/\<(?:hr|img|h\d|strong|em|strikethrough|table|a|b|i|del|ol|ul)(?:\s.*?|\/)?\>/i.test(html)) return null;
+
+  const turndownService = getTurndownService();
+  if (turndownService) {
+    let markdown = turndownService.turndown(html);
+
+    // Convert spaces to tabs for consistency across different editors
+    markdown = markdown.replace(/^ {4}/gm, "\t")
+                       .replace(/^ {3}/gm, "\t")
+                       .replace(/^(?!\t)( {2})/gm, "\t");
+    return markdown;
   }
 
+  return null;
+};
+
 export const getTurndownService = (function () {
-  var service: TurndownService = null
+  var service: TurndownService = null;
 
   return function () {
     if (!service && typeof TurndownService === 'function') {
@@ -55,21 +55,31 @@ export const getTurndownService = (function () {
         "strongDelimiter": "**",
         "linkStyle": "inlined",
         "linkReferenceStyle": "collapsed"
-      }
-      service = new TurndownService(opts)
+      };
 
+      service = new TurndownService(opts);
+
+      // Use turndownPluginGfm to enable GFM features including tables
       if (typeof turndownPluginGfm !== 'undefined') {
-        service.use(turndownPluginGfm.gfm)
-      }
+        var gfm = turndownPluginGfm.gfm;
+        var tables = turndownPluginGfm.tables;
+        var strikethrough = turndownPluginGfm.strikethrough;
 
+        // Use the gfm plugin
+        service.use(gfm);
+
+        // Use the table and strikethrough plugins only
+        service.use([tables, strikethrough]);
+      }
     }
-    return service
-  }
-})()
+
+    return service;
+  };
+})();
 
 if (typeof TurndownService != "undefined") {
-  // Use this convertor as default convertor
-  defaultOption.convertor = TurndownConvertor
+  // Use this convertor as the default convertor
+  defaultOption.convertor = TurndownConvertor;
 } else {
-  console.error("[HyperMD] PowerPack paste-with-turndown loaded, but turndown not found.")
+  console.error("[HyperMD] PowerPack paste-with-turndown loaded, but turndown not found.");
 }
